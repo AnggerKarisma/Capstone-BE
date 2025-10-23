d00ecb73bc2f278fe42d1f84a82da195bf4bc75d
fadilahmalfoy <wfadillah863@gmail.com>
2025-10-23 17:57:28 +0800

    route api

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,7 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 
 //Akses khusus superadmin
 Route::middleware(['auth:sanctum', 'role:superadmin'])->group(function () {
-    
-    // CRUD Admin
+
     Route::get('/admins', [AdminController::class, 'index']);      
     Route::post('/admins', [AdminController::class, 'store']);    
     Route::delete('/admins/{id}', [AdminController::class, 'destroy']); 
@@ -20,8 +19,6 @@ Route::middleware(['auth:sanctum', 'role:superadmin'])->group(function () {
     Route::post('/polis', [PoliController::class, 'store']);
     Route::put('/polis/{id}', [PoliController::class, 'update']);
     Route::delete('/polis/{id}', [PoliController::class, 'destroy']);    
-    // CRUD Poli
-    // Route::apiResource('polis', PoliController::class);
 });
 
 //Akses khusus admin 
@@ -35,7 +32,6 @@ Route::middleware(['auth:sanctum', 'role:superadmin,admin'])->group(function () 
     Route::get('/admins/{id}', [AdminController::class, 'show']); 
     Route::put('/admins/{id}', [AdminController::class, 'update']); 
 
-    // CRUD Poli yang bisa diakses semua
     Route::get('/polis', [PoliController::class, 'index']);
     Route::get('/polis/{id}', [PoliController::class, 'show']);
 });
